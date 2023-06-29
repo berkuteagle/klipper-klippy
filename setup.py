47a7677d69ba1ba8f2f7151fe76c59900116e24a
Anton <berkuteagle@gmail.com>
2023-06-29 21:52:19 +0500

    Fix build v2

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
               "klippy.extras", "klippy.extras.display"],
     package_dir={"klippy": "src"},
     package_data={"klippy.extras": ["*.cfg"],
+                  "klippy": ["chelper/*.c", "chelper/*.h"],
                   "klippy.extras.display": ["*.cfg"]},
     cffi_modules=["src/chelper_builder.py:ffibuilder"],
     install_requires=["cffi>=1.0.0"]
